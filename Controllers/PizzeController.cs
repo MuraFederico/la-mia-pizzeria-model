a6c38609092a99de82b279d407d92ba2481fbc9d
MuraFederico <federicomura98@gmail.com>
2022-07-15 15:28:54 +0200

    created database and added Details view

---
+++ Controllers/PizzeController.cs
@@ -0,0 +1,24 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzeController : Controller
+    {
+        public IActionResult Index()
+        {
+            PizzaContext context = new PizzaContext();
+            
+            IQueryable<Pizza> pizzas = context.Pizzas;
+            return View(pizzas);
+            
+        }
+
+        public IActionResult Details(int id)
+        {
+            PizzaContext context = new PizzaContext();
+            Pizza pizza = context.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+            return View(pizza);
+        }
+    }
+}
---
+++ PizzaContext.cs
@@ -0,0 +1,17 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=PizzaDb;Integrated Security=True");
+        }
+    }
+
+    
+}
